61c1a6b94eb8678e05796680e1c62691870559b9
Alec Chan <Dev_ACH@DEV.tpt.org>
2018-06-05 17:32:11 +0100

    test2

--- Controllers/BuildController.cs
+++ Controllers/BuildController.cs
@@ -31,31 +31,57 @@ public class BuildController : Controller {
     }
 
     [Produces ("application/json")]
-    public IActionResult Data () {
+    public IActionResult Data (DateTime? start = null, DateTime? end = null) {
 
-        var benchmarks = _repo.GetBuildBenchmarks (DateTime.Now.StartOfWeek (DayOfWeek.Saturday), DateTime.Now)
-                    .Where (x => x.SolutionBuild == false && x.UpToDate == false );
+        start = start ?? DateTime.Now.StartOfWeek (DayOfWeek.Saturday);
+        end = end ?? DateTime.Now;
 
-        var projects = benchmarks            
+        var benchmarks = _repo.GetBuildBenchmarks (start.Value, end.Value)
+            .Where (x => x.SolutionBuild == false && x.UpToDate == false);
+
+        var projects = benchmarks
             .GroupBy (g => g.Tags)
             .Select (s => new { Tags = s.Key, Count = s.Count () })
             .OrderByDescending (x => x.Count)
             .Select (s => s.Tags);
 
-        var data = new Dictionary<string, List<BuildData>>();
+        var data = new Dictionary<string, List<BuildData>> ();
 
         foreach (var p in projects) {
-            
-            var items = benchmarks.Where(x=>x.Tags == p ).Select(x=> new BuildData {
+
+            var items = benchmarks.Where (x => x.Tags == p).Select (x => new BuildData {
                 ProjectName = x.Tags,
-                Host = x.Host,
-                Duration = x.BuildDuration,
-                CreatedDate = x.CreatedDate
+                    Host = x.Host,
+                    Duration = x.BuildDuration,
+                    CreatedDate = x.CreatedDate
             });
-            
-            data.Add(p, items.ToList());
+
+            data.Add (p, items.ToList ());
         }
 
         return Json (data.ToArray ());
     }
+
+    [Produces ("application/json")]
+    [Route ("Build/Data/Project")]
+    public IActionResult BuildByProject (DateTime? start = null, DateTime? end = null) {
+
+        start = start ?? DateTime.Now.StartOfWeek (DayOfWeek.Saturday);
+        end = end ?? DateTime.Now;
+
+        var benchmarks = _repo.GetBuildBenchmarks (start.Value, end.Value)
+            .Where (x => x.SolutionBuild == true && x.UpToDate == false);
+
+        var projects = from b in benchmarks
+            group b by b.Solution into grp
+            select new {
+                Solution = grp.Key,
+                ProjectCount = benchmarks.Where(b=>b.Solution == grp.Key).Select(x=>x.Tags).Distinct().Count(),
+                BuildCount = grp.Count (),
+                TotalBuildDuration = grp.Sum (c => c.BuildDuration)
+            };
+
+        return Json (projects.ToArray ());
+    }
+
 }
